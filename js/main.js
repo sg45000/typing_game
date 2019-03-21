85532c8b8da6cbb8ffc915c321e1b630e1d34c03
sg45000 <hamakyo711@gmail.com>
2019-03-21 21:45:52 +0900

    complete modeified

--- js/main.js
+++ js/main.js
@@ -8,19 +8,63 @@
     'great',
     'peach',
     'speed',
-    'divide'
+    'divide',
+    'height',
+    'chicken'
   ];
-  let word = 'apple';
-  let loc = 0;
-  let score = 0;
-  let miss = 0;
+  let word = 'click to start';
+  let loc ;
+  let score ;
+  let miss ;
+  const timeLimit = 10 * 1000;
   const target = document.getElementById('target');
   const scoreLabel = document.getElementById('score');
   const missLabel = document.getElementById('miss');
-  target.textContent = word;
+  const timerLabel = document.getElementById('timer');
+  let startTime;
+  let isPlaying = false;
 
+  function showResult(){
+    const accuracy = score + miss === 0 ? 0 : score/(score + miss) * 100;
+    alert(`${score} letters, ${miss} misses ,${accuracy.toFixed(2)}% accuracy`);
+  }
+
+  function updateTimer(){
+    const timeLeft = startTime + timeLimit - Date.now();
+    timerLabel.textContent = (timeLeft/1000).toFixed(2);
+
+    const timeoutId = setTimeout(()=>{
+      updateTimer();
+    },10)
+
+    if(timeLeft < 0){
+      isPlaying= false;
+      clearTimeout(timeoutId);
+      timerLabel.textContent='0.00';
+      setTimeout(()=>{
+        showResult();
+      },100)
+
+      timerLabel.textContent = '0.00';
+      target.textContent = 'click to replay'
+    }
+  }
+
+  window.addEventListener('click',() => {
+    if(isPlaying=== true){
+      return;
+    }
+    isPlaying=true;
+    score = 0;
+    miss = 0;
+    scoreLabel.textContent = score;
+    missLabel.textContent = miss;
+    nextWord();
+    startTime = Date.now();
+    updateTimer();
+  })
   function nextWord(){
-    word= words[Math.floor(Math.random()*word.length)];
+    word= words[Math.floor(Math.random()*words.length)];
     target.textContent = word;
     loc = 0;
   }
@@ -34,6 +78,9 @@
   }
 
   window.addEventListener('keyup', e =>{
+    if(isPlaying!== true){
+      return;
+    }
     if(e.key === word[loc]){
       loc ++;
       score++;
